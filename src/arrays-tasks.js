8affa4cb2005ff9a901ab741df16b47c1444c2ac
kagerka <kagerka@gmail.com>
2024-01-09 16:22:49 +0300

    feat: add solution to getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -139,8 +139,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+  const result = (arr.reduce((a, b) => a + b, 0) / arr.length).toFixed(2);
+  return +result;
 }
 
 /**
