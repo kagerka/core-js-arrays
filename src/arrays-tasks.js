0b849dee116f459c21035030ddfa8527f39767dc
kagerka <kagerka@gmail.com>
2024-01-09 21:44:32 +0300

    feat: add solution to generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -379,8 +379,12 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const result = [];
+  for (let i = 0, num = 1; i < len; i += 1, num += 2) {
+    result.push(num);
+  }
+  return result;
 }
 
 /**
