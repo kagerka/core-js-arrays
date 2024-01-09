162d2e0f3c419183e434e264e92f415812a40c1b
kagerka <kagerka@gmail.com>
2024-01-09 16:15:40 +0300

    feat: add solution to getStringsLength task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -120,8 +120,9 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  const result = arr.map((i) => i.length);
+  return result;
 }
 
 /**
